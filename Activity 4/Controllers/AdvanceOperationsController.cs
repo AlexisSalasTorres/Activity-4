468d0a3f984628cc44948599f5556a847740908f
Admin <Admin@EA611-22>
2025-02-06 20:52:24 +0800

    Add project files.

---
+++ Activity 4/Controllers/AdvanceOperationsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Activity_4.Controllers
+{
+    [Route("AdvancedOperations")]
+    public class AdvanceOperationsController : Controller
+    {
+        [HttpGet("Square/{num}")]
+        public IActionResult Square(double num)
+        {
+            double result = num * num;
+            return Content($"Square of {num}: {result}");
+        }
+
+        [HttpGet("SquareRoot/{num}")]
+        public IActionResult SquareRoot(double num)
+        {
+            if (num < 0)
+                return Content("Error: Cannot calculate square root of a negative number.");
+
+            double result = Math.Sqrt(num);
+            return Content($"Square root of {num}: {result}");
+        }
+
+        [HttpGet("Power/{baseNum}/{exponent}")]
+        public IActionResult Power(double baseNum, double exponent)
+        {
+            double result = Math.Pow(baseNum, exponent);
+            return Content($"{baseNum} raised to the power of {exponent}: {result}");
+            
+        }
+    }
+}
---
+++ Activity 4/Controllers/OperationsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Activity_4.Controllers
+{
+    [Route("Operations")]
+    public class OperationsController : Controller
+    {
+        [HttpGet("{operation}/{num1}/{num2}")]
+        public IActionResult Calculate(string operation, double num1, double num2)
+        {
+            double result;
+
+            switch (operation.ToLower())
+            {
+                case "add":
+                    result = num1 + num2;
+                    break;
+                case "subtract":
+                    result = num1 - num2;
+                    break;
+                case "multiply":
+                    result = num1 * num2;
+                    break;
+                case "divide":
+                    if (num2 == 0)
+                        return Content("Error: Division by zero is not allowed.");
+                    result = num1 / num2;
+                    break;
+                default:
+                    return Content("Error: Invalid operation. Use Add, Subtract, Multiply, or Divide.");
+            }
+
+            return Content($"Result of {operation}: {result}");
+        }
+    } 
+}
